daec8665f1b59d5f4cebd332e2bdd6117e5db9d9
Aragorn Huang <karateboy.tw@gmail.com>
2019-10-18 13:44:52 +0800

    1. export to local Modbus slave 2. Add Modbus slave simulator

--- app/models/Exporter.scala
+++ app/models/Exporter.scala
@@ -4,6 +4,8 @@ import java.nio.file.{ Paths, Files, StandardOpenOption }
 import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.github.nscala_time.time.Imports._
+import scala.concurrent._
+import ModelHelper._
 object Exporter {
   val activeMonitorType = List(MonitorType.mtvList: _*)
   def exportActiveMonitorType = {
@@ -19,6 +21,69 @@ object Exporter {
   }
 
   var latestDateTime = new DateTime(0)
+  import com.serotonin.modbus4j._
+  var masterOpt: Option[ModbusMaster] = None
+
+  def writeModbusSlave(data: Record.RecordList) = {
+    import com.serotonin.modbus4j.ip.IpParameters
+
+    def connectHost() {
+      val ipParameters = new IpParameters()
+      ipParameters.setHost("127.0.0.1");
+      ipParameters.setPort(502);
+      val modbusFactory = new ModbusFactory()
+
+      val master = modbusFactory.createTcpMaster(ipParameters, true)
+      master.setTimeout(4000)
+      master.setRetries(1)
+      master.setConnected(true)
+      master.init();
+      masterOpt = Some(master)
+    }
+
+    def writeReg(master: ModbusMaster) = {
+      import com.serotonin.modbus4j.msg._
+      import com.serotonin.modbus4j.locator.BaseLocator
+      import com.serotonin.modbus4j.code.DataType;
+      import java.nio._
+      val slaveID = 1
+
+      def writeShort(offset: Int, value: Short) = {
+        val request = new WriteRegisterRequest(slaveID, offset, value)
+        master.send(request)
+      }
+      def writeLong(offset: Int, value: Long) = {
+        val locator = BaseLocator.holdingRegister(slaveID, offset, DataType.EIGHT_BYTE_INT_SIGNED);
+        master.setValue(locator, value);
+      }
+
+      def writeDouble(offset: Int, value: Double) = {        
+        val locator = BaseLocator.holdingRegister(slaveID, offset, DataType.EIGHT_BYTE_FLOAT);
+        master.setValue(locator, value);
+      }
+
+      //Selector
+      writeShort(0, Selector.get.toShort)
+      for ((mtData, idx) <- data.mtDataList.zipWithIndex) {
+        writeDouble(idx * 4 + 1, mtData.value)
+      }
+
+    }
+
+    Future {
+      blocking {
+        try {
+          if (masterOpt.isEmpty)
+            connectHost
+
+          masterOpt map { writeReg }
+        } catch {
+          case ex: Exception =>
+            Logger.error(ex.getMessage, ex)
+        }
+      }
+    } onFailure errorHandler
+  }
 
   def exportRealtimeData = {
     val path = Paths.get(current.path.getAbsolutePath + "/export/realtime.txt")
@@ -35,10 +100,14 @@ object Exporter {
         } else {
           records.head
         }
+
       val dateTime = new DateTime(data.time)
       if (latestDateTime < dateTime) {
         latestDateTime = dateTime
-
+        
+        //Export to modbus
+        writeModbusSlave(data)
+        
         buffer += s"InjectionDate, ${data.time}\r"
         val mtStrs = data.mtDataList map { mt_data => s"${mt_data.mtName}, ${mt_data.value}" }
         val mtDataStr = mtStrs.foldLeft("")((a, b) => {
@@ -48,7 +117,7 @@ object Exporter {
             a + "\r" + b
         })
         buffer += mtDataStr
-        Files.write(path, buffer.getBytes, StandardOpenOption.CREATE)
+        val ret = Files.write(path, buffer.getBytes, StandardOpenOption.CREATE, StandardOpenOption.SYNC, StandardOpenOption.TRUNCATE_EXISTING)
         true
       } else
         false
